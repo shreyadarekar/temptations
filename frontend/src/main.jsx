c0873c9495e368334349a37de6ae37e8090943e4
Shreya <shreya.r.darekar@gmail.com>
2024-01-31 18:45:13 -0800

    add user login and signup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
+// frontend/src/main.jsx
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
+import { Provider } from 'react-redux';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
+import { Modal, ModalProvider } from './context/Modal';
 
 const store = configureStore();
 
@@ -13,12 +17,16 @@ if (import.meta.env.MODE !== 'production') {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ModalProvider>
+      <Provider store={store}>
+        <App />
+        <Modal />
+      </Provider>
+    </ModalProvider>
   </React.StrictMode>
 );
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,8 +1,9 @@
 import { applyMiddleware, compose, combineReducers, createStore } from "redux";
 import thunk from "redux-thunk";
+import sessionReducer from "./session";
 
 const rootReducer = combineReducers({
-  // ADD REDUCERS HERE
+  session: sessionReducer,
 });
 
 let enhancer;

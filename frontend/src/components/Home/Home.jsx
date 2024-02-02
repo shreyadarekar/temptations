3d84152b0ffdd17f7b08f241887d44b25dced717
Shreya <shreya.r.darekar@gmail.com>
2024-02-01 21:46:29 -0800

    add a home page with logo

---
+++ frontend/src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home-container">
+      <img className="logo" src="../../logo.png" alt="logo" />
+      <h1>Temptations</h1>
+    </div>
+  );
+}
+
+export default Home;

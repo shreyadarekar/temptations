c8baba8e6af35ad8e2ca7457181e9ae5b6d49035
Shreya <shreya.r.darekar@gmail.com>
2024-02-20 14:29:56 -0800

    render sql fix

--- backend/db/migrations/20240204225345-create-cookbook.js
+++ backend/db/migrations/20240204225345-create-cookbook.js
@@ -8,36 +8,41 @@ if (process.env.NODE_ENV === "production") {
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Cookbooks", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
+    await queryInterface.createTable(
+      "Cookbooks",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        userId: {
+          allowNull: false,
+          type: Sequelize.INTEGER,
+          references: { model: "Users" },
+          onDelete: "CASCADE",
+        },
+        title: {
+          allowNull: false,
+          type: Sequelize.STRING(100),
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
       },
-      userId: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-        references: { model: "Users" },
-        onDelete: "CASCADE",
-      },
-      title: {
-        allowNull: false,
-        type: Sequelize.STRING(100),
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-      },
-    });
+      options
+    );
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("Cookbooks");
+    options.tableName = "Cookbooks";
+    await queryInterface.dropTable(options);
   },
 };

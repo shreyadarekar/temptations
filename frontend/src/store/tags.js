f5d01cb7f8556d35226041a7deabc94a7f7a2319
Shreya <shreya.r.darekar@gmail.com>
2024-02-22 00:13:28 -0800

    add tags const

---
+++ frontend/src/store/tags.js
@@ -0,0 +1,12 @@
+export const TAGS = [
+  { label: "Indian", value: "indian" },
+  { label: "American", value: "american" },
+  { label: "Italian", value: "italian" },
+  { label: "Chinese", value: "chinese" },
+  { label: "Thai", value: "thai" },
+  { label: "Korean", value: "korean" },
+  { label: "Seafood", value: "seafood" },
+  { label: "Gluten-free", value: "gluten-free" },
+  { label: "Vegan", value: "vegan" },
+  { label: "Non-dairy", value: "non-dairy" },
+];

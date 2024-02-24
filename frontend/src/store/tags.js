842e6a8715eaa6746cca605bd4f45cea2c1541d6
Shreya <shreya.r.darekar@gmail.com>
2024-02-23 23:42:37 -0800

    update tags

--- frontend/src/store/tags.js
+++ frontend/src/store/tags.js
@@ -5,8 +5,12 @@ export const TAGS = [
   { label: "Chinese", value: "chinese" },
   { label: "Thai", value: "thai" },
   { label: "Korean", value: "korean" },
+  { label: "Japanese", value: "japanese" },
+  { label: "Mexican", value: "mexican" },
   { label: "Seafood", value: "seafood" },
   { label: "Gluten-free", value: "gluten-free" },
   { label: "Vegan", value: "vegan" },
   { label: "Non-dairy", value: "non-dairy" },
+  { label: "Healthy", value: "healthy" },
+  { label: "Dessert", value: "dessert" },
 ];
